b6d7e004aea86f933ca19b1a475e90cf6811dc9b
camer0nluo <cameronluo@gatech.edu>
2021-01-16 20:14:33 +0800

    More Edits to Chatbot

--- app.js
+++ app.js
@@ -23,28 +23,114 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post("/callback", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+  // req.body.events should be an array of events
+  if (!Array.isArray(req.body.events)) {
+    return res.status(500).end();
+  }
+  // handle events separately
+  Promise.all(
+    req.body.events.map((event) => {
+      console.log("event", event);
+      // check verify webhook event
+      if (
+        event.replyToken === "00000000000000000000000000000000" ||
+        event.replyToken === "ffffffffffffffffffffffffffffffff"
+      ) {
+        return;
+      }
+      return handleEvent(event);
+    })
+  )
+    .then(() => res.end())
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
 });
 
-// event handler
+// simple reply function
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: "text", text }))
+  );
+};
+
 function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+  switch (event.type) {
+    case "message":
+      const message = event.message;
+      switch (message.type) {
+        case "text":
+          return handleText(message, event.replyToken);
+        case "image":
+          return handleImage(message, event.replyToken);
+        case "video":
+          return handleVideo(message, event.replyToken);
+        case "audio":
+          return handleAudio(message, event.replyToken);
+        case "location":
+          return handleLocation(message, event.replyToken);
+        case "sticker":
+          return handleSticker(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case "follow":
+      return replyText(event.replyToken, "Got followed event");
+
+    case "unfollow":
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case "join":
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+    case "leave":
+      return console.log(`Left: ${JSON.stringify(event)}`);
+
+    case "postback":
+      let data = event.postback.data;
+      return replyText(event.replyToken, `Got postback: ${data}`);
+
+    case "beacon":
+      const dm = `${Buffer.from(event.beacon.dm || "", "hex").toString(
+        "utf8"
+      )}`;
+      return replyText(
+        event.replyToken,
+        `${event.beacon.type} beacon hwid : ${event.beacon.hwid} with device message = ${dm}`
+      );
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
-
-  // create a echoing text message
-  const echo = { type: "text", text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
 }
 
+function handleText(message, replyToken) {
+  return replyText(replyToken, message.text);
+}
+
+function handleImage(message, replyToken) {
+  return replyText(replyToken, "Got Image");
+}
+
+function handleVideo(message, replyToken) {
+  return replyText(replyToken, "Got Video");
+}
+
+function handleAudio(message, replyToken) {
+  return replyText(replyToken, "Got Audio");
+}
+
+function handleLocation(message, replyToken) {
+  return replyText(replyToken, "Got Location");
+}
+
+function handleSticker(message, replyToken) {
+  return replyText(replyToken, "Got Sticker");
+}
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
